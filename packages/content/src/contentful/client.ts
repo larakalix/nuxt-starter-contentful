48dba3e2541182330698c3036129f758bbd023d6
Ivan-Lara_mfsg <ivan.lara@moneymart.ca>
2025-12-13 14:35:14 -0500

    chore: env references added in turbo json

--- packages/content/src/contentful/client.ts
+++ packages/content/src/contentful/client.ts
@@ -51,6 +51,11 @@ export function createContentfulClient(preview = false) {
         previewToken,
     } = config;
 
+    console.log("[ENV_VARS] -> ", {
+        spaceId,
+        cdaToken,
+    });
+
     if (!spaceId || !cdaToken) {
         console.warn(
             "[contentful] spaceId/cdaToken missing. Contentful queries will fail."
