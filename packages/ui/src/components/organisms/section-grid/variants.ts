09fbf2d24d00e9a8544f20a350e2c4460e05eb8a
Ivan-Lara_mfsg <ivan.lara@moneymart.ca>
2025-12-17 02:07:03 -0500

    fix: col span style for mobile fixed

--- packages/ui/src/components/organisms/section-grid/variants.ts
+++ packages/ui/src/components/organisms/section-grid/variants.ts
@@ -5,11 +5,13 @@ export const sectionGridVariants = tv({
     variants: {
         columns: {
             1: "grid-cols-1",
-            2: "grid-cols-2",
-            3: "grid-cols-3",
-            4: "grid-cols-4",
-            5: "grid-cols-5",
-            6: "grid-cols-6",
+            2: "grid-cols-1 md:grid-cols-2",
+            3: "grid-cols-1 md:grid-cols-2 xl:grid-cols-3",
+            4: "grid-cols-1 md:grid-cols-2 xl:grid-cols-4",
+            5: "grid-cols-1 md:grid-cols-2 xl:grid-cols-5",
+            6: "grid-cols-1 md:grid-cols-2 xl:grid-cols-6",
+            7: "grid-cols-1 md:grid-cols-2 xl:grid-cols-7",
+            8: "grid-cols-1 md:grid-cols-2 xl:grid-cols-8",
         },
     },
     defaultVariants: {},
